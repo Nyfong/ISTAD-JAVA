d79867f2fcfe4ce4afaf6e746006f413490be471
Nyfong <litongfong12@gmail.com>
2024-11-18 16:25:11 +0700

    hello world

--- src/introduction/helloJava.java
+++ src/introduction/helloJava.java
@@ -3,7 +3,8 @@ package introduction;
 public class helloJava {
     public  static  void main(String[] args)
     {
-        System.out.println("Hello one");
+        String name = "nyfong";
+        System.out.println("Hello ISTAD my name is "+name+" .");
     }
 
 }
