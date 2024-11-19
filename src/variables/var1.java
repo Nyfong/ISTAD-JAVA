a588704aab3f0a57f9f2eb2cf2ad563c6c3f3111
Nyfong <litongfong12@gmail.com>
2024-11-19 13:24:10 +0700

    variable1

---
+++ src/variables/var1.java
@@ -0,0 +1,8 @@
+package variables;
+
+public class var1 {
+    public static  void main(String[] args){
+        int number = 10;
+        System.out.println("the number value is "+number);
+    }
+}
