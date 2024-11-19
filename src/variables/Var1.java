d0d3ea4f31c2b289421c7db99af94c06d5552527
Nyfong <litongfong12@gmail.com>
2024-11-19 14:56:20 +0700

    commit Classes wrapper

---
+++ src/variables/Var1.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Var1 {
+    public static  void main(String[] args){
+        int number = 10; //premetive datatype
+        Integer numObject = number;  // auto-box Classes wrapper
+        System.out.println(numObject.toString()+1);
+
+//        System.out.println("the number value is "+number);
+    }
+}
+
+//Every line of code that runs in Java must be inside a class.
+// And the class name should always start with an uppercase first letter.
+
+
--- src/variables/var1.java
+++
@@ -1,8 +0,0 @@
-package variables;
-
-public class var1 {
-    public static  void main(String[] args){
-        int number = 10;
-        System.out.println("the number value is "+number);
-    }
-}
