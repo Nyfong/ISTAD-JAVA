98e83de63b388c8e6cb9a44eda47594571d8c3fc
Nyfong <litongfong12@gmail.com>
2024-11-20 16:15:36 +0700

    kdet variables

--- src/variables/Var3.java
+++ src/variables/Var3.java
@@ -19,5 +19,11 @@ public class Var3 {
         System.out.println(name+" "+age+" "+hour);
         System.out.println(userName+""+ageKdet+" "+hourKdet);
 
+        String _puKdet = " Kdet saouy nas ";
+        System.out.println(_puKdet);
+
+        int $kdetSize = 33;
+        System.out.println($kdetSize);
+
     }
 }
