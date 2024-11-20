af8a495b0dd2b219856d8ee575a5604b54e86d3c
Nyfong <litongfong12@gmail.com>
2024-11-20 16:13:37 +0700

    kdet variables

--- src/variables/Var2.java
+++ src/variables/Var2.java
@@ -9,13 +9,18 @@ public class Var2 {
     public static double calculateCubeSurfaceArea(double side) {
         return 6 * Math.pow(side, 2);
     }
+
     // Method to calculate the surface area of a sphere
     public static double calculateSpharedSurfaceArea(double raduis) {
-        return 4 * Math.PI *Math.pow(raduis, 2);
+        return 4 * Math.PI * Math.pow(raduis, 2);
     }
-    private  static final DecimalFormat  df = new DecimalFormat("0.0");
+
+    private static final DecimalFormat df = new DecimalFormat("0.0");
+
     public static void main(String[] args) {
+
         df.setRoundingMode(RoundingMode.DOWN);
+
         // Cube example
         double cubeSide = 5.0;  // Length of side of the cube
         double cubeSurfaceArea = calculateCubeSurfaceArea(cubeSide);
@@ -25,8 +30,6 @@ public class Var2 {
         double sphereRadius = 7.0;  // Radius of the sphere
         double sphereSurfaceArea = calculateSpharedSurfaceArea(sphereRadius);
         System.out.println("Surface Area of Sphere with radius " + df.format(sphereRadius) + ": " + df.format(sphereSurfaceArea));
-
-
 
     }
 
---
+++ src/variables/Var3.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Var3 {
+    public static void main(String[] args) {
+
+        //variables declaretion
+        String name;
+        int age;
+        double hour;
+        //assignment
+        name = "ny Fong";
+        age = 19;
+        hour = 15.4;
+        //initailization
+        String userName = "kdet";
+        int ageKdet = 20;
+        double hourKdet = 15.4;
+
+        System.out.println(name+" "+age+" "+hour);
+        System.out.println(userName+""+ageKdet+" "+hourKdet);
+
+    }
+}
