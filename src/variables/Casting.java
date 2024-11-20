ea2de19b897a9e83eb9e32ae5dd322cab40beca4
Nyfong <litongfong12@gmail.com>
2024-11-21 00:24:18 +0700

    casting Floating to an integer

---
+++ src/variables/Casting.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Casting {
+    public static void main(String[] args)
+    {
+        double kdetKo = 2.4f;
+
+        //casting premetive datatype
+        int  kdetChma = (int)kdetKo;
+        System.out.println(kdetKo);
+        System.out.println(kdetChma);
+
+    }
+
+}
