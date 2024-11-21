584deee16777863aad636b17c1b31fac7bafb45b
Nyfong <litongfong12@gmail.com>
2024-11-21 12:33:41 +0700

    kdetKo casting

--- src/variables/Casting.java
+++ src/variables/Casting.java
@@ -3,11 +3,15 @@ package variables;
 public class Casting {
     public static void main(String[] args)
     {
-        double kdetKo = 2.4f;
+        double kdetKo;
+        kdetKo = 2.4f;
 
         //casting premetive datatype
-        int  kdetChma = (int)kdetKo;
+        int  kdetChma ;
+        kdetChma = (int)kdetKo;
+
         System.out.println(kdetKo);
+        //casting kdetKo ->  to integer assigned to -> kdetChma
         System.out.println(kdetChma);
 
     }
