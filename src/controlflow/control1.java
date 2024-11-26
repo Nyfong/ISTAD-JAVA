0ddaca6fb5601eac1ad98a95812a5b8e2313a04f
Nyfong <litongfong12@gmail.com>
2024-11-26 15:07:28 +0700

    ternary 1

---
+++ src/controlflow/control1.java
@@ -0,0 +1,10 @@
+package controlflow;
+
+public class control1 {
+    public static void main(String[] args) {
+
+            char Gender = 'M';
+            String answer = (Gender == 'F') ? "Female" : "Male";
+            System.out.println(answer);
+    }
+}
