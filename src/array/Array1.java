3f06cd4eca61bc47cd2a2ff544c8c230e94210d4
Nyfong <litongfong12@gmail.com>
2024-12-03 16:06:23 +0700

    algorithms simple

--- src/array/Array1.java
+++ src/array/Array1.java
@@ -6,23 +6,26 @@ public class Array1 {
         //Declaring array literal
 
         int[] arr = {1,2,3,10,4,5,12}; //five elements
+        System.out.println(Arrays.toString(arr));
         //finding Max
         int temp =0;
         for(int i =0 ; i< arr.length; i+=1)
-        {
             for(int j =0 ; j< arr.length; j+=1)
-            {
-                if(arr[j] > arr[i])
-                {
-                     temp = arr[j];
-                }
-            }
-        }
+                if(arr[j] > arr[i])   temp = arr[j];
+
+
         System.out.println(temp +" : Max value");
-
-
-
-
+        int sum =0;
+        //finding sum
+        for(var el : arr)
+        {
+            sum += el;
+        }
+        System.out.println("Sum:" + sum);
+        //finding Avg
+        int avg ;
+        avg = sum/arr.length;
+        System.out.println("Average: "+avg);
 
 
     }
