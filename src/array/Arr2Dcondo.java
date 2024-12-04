5cfcc9524ef48d4397b8779c57f7650a80d69b6c
Nyfong <litongfong12@gmail.com>
2024-12-04 15:02:45 +0700

    arr2Dcondo

---
+++ src/array/Arr2Dcondo.java
@@ -0,0 +1,58 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Arr2Dcondo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int floorsNumber, roomsNumber;
+        String userName="";
+        System.out.println("----- Construction the Condo -------");
+        System.out.print("Please enter floors number : ");
+         floorsNumber = sc.nextInt();
+        System.out.print("Please enter rooms number : ");
+         roomsNumber = sc.nextInt();
+        String condo[][] = new String[floorsNumber][roomsNumber];
+        System.out.println("The condo have"+ Arrays.deepToString(condo));
+
+        int option =0;
+        //programs
+        do{
+            System.out.println("----- Welcome to Condo -------");
+            System.out.println("[1].  Buy condo");
+            System.out.println("[2].  Display condo information");
+            System.out.println("[3].  Exit the program");
+            System.out.print("Please enter option : ");
+            option = sc.nextInt();
+
+            switch (option)
+            {
+                case 1 ->{
+
+                    System.out.print("Please enter floors number : ");
+                    floorsNumber = sc.nextInt();
+                    System.out.print("Please enter rooms number : ");
+                    roomsNumber = sc.nextInt();
+                    // Clear the buffer after nextInt()
+                    sc.nextLine();
+                    System.out.print("Enter your name:");
+                    userName = sc.nextLine();
+                    condo[floorsNumber -1][roomsNumber -1 ] = userName;
+                }
+                case 2 ->{
+                    System.out.println("Display all the condo information");
+                    for( var floors : condo)
+                        for( var rooms : floors) System.out.println(rooms);
+                }
+                case 3 -> {
+                    option =-1;
+                    System.out.println("Exit the program");
+                }
+                default -> System.out.println("Invalid option");
+            }
+
+        }while(option >0  && option < 4);
+
+    }
+}
