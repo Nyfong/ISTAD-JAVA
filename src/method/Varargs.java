a64dfa90918135d8b495b0b03109efac3a5a236a
Nyfong <litongfong12@gmail.com>
2024-12-08 19:33:12 +0700

    commmit the varags in the java ...

---
+++ src/method/Varargs.java
@@ -0,0 +1,34 @@
+package method;
+
+public class Varargs {
+
+    // method use for sum the value
+    public  static  int sumAnInteger (int ... number)
+    {
+        int sum = 0;
+      for( var i : number)
+      {
+          sum += i;
+      }
+      return sum;
+    }
+    public static void main(String[] args) {
+
+        //print the result
+        System.out.println(sumAnInteger(1,2));
+        System.out.println(sumAnInteger(1,2,3,4,5));
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
