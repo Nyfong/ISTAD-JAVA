d5fc91b61b80c970424e7aa2b9781d95f1359dd2
Nyfong <litongfong12@gmail.com>
2024-12-09 15:39:47 +0700

    validation

--- src/array/Arr2Dcondo.java
+++ src/array/Arr2Dcondo.java
@@ -15,11 +15,14 @@ public class Arr2Dcondo {
          roomsNumber = sc.nextInt();
         String condo[][] = new String[floorsNumber][roomsNumber];
         System.out.println("The condo have"+ Arrays.deepToString(condo));
+        String GREEN = "\u001B[32m";
+        String RED = "\u001B[31m";
+        String RESET = "\u001B[0m";
 
         int option =0;
         //programs
         do{
-            System.out.println("----- Welcome to Condo -------");
+            System.out.println("\n----- Welcome to Condo -------");
             System.out.println("[1].  Buy condo");
             System.out.println("[2].  Display condo information");
             System.out.println("[3].  Exit the program");
@@ -28,26 +31,39 @@ public class Arr2Dcondo {
 
             switch (option)
             {
-                case 1 ->{
 
-                    System.out.print("Please enter floors number : ");
-                    floorsNumber = sc.nextInt();
-                    System.out.print("Please enter rooms number : ");
-                    roomsNumber = sc.nextInt();
-                    // Clear the buffer after nextInt()
-                    sc.nextLine();
-                    System.out.print("Enter your name:");
-                    userName = sc.nextLine();
-                    condo[floorsNumber -1][roomsNumber -1 ] = userName;
+                case 1 ->{
+                    boolean back = true;
+                    while (back) {
+                        System.out.print("Please enter floors number: ");
+                        floorsNumber = sc.nextInt();
+                        System.out.print("Please enter rooms number: ");
+                        roomsNumber = sc.nextInt();
+
+                        // Validate floor and room numbers
+                        if (floorsNumber > 0 && floorsNumber <= condo.length && roomsNumber > 0 && roomsNumber <= condo[floorsNumber - 1].length) back = false; // Valid input, exit loop
+                         else System.out.println(RED+"Invalid floor or room number. Please try again."+RESET);
+                        // Clear the buffer after nextInt()
+                        sc.nextLine();
+
+                        // If valid, prompt for user name
+                        if (back == false) {
+                            System.out.print(GREEN+"Enter your name: "+RESET);
+                            userName = sc.nextLine();
+                            condo[floorsNumber - 1][roomsNumber - 1] = userName; // Assign user name to the condo array
+                        }
+                    }
                 }
                 case 2 ->{
                     int j=0;
-                    System.out.println("Display all the condo information");
+                    System.out.println("---------- ***** Display all the condo information ***** ----------");
                     for( var floors : condo) {
                         int i=0;
                         j+=1;
-                        for( var rooms : floors) System.out.println((rooms == null)?   "floor: "+(j)+", room:"+(i+=1)+". not yet have owner": "floor: "+(j)+", room:"+(i+=1) +". " + rooms +" 's room ");
-
+                        System.out.println();
+                        for( var rooms : floors) System.out.print((rooms == null)?
+                                "floor:[ "+(j)+"], room:"+(i+=1)+ GREEN +"[ not yet have owner ]  "+  RESET+",":
+                                "floor:[ "+(j)+"], room:"+(i+=1) + RED +"[ ** " + rooms +" 's room ] "+ RESET+",");
                     }
 
                 }
